607624724d9f286252b03ea6952452764cf1c063
Romy Owens <rdoed160@gmail.com>
2019-09-26 21:46:38 -0400

    added code for display wins/loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,48 @@
-var computerGuess = "a";
+var computerGuess = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 var wins = 0;
 var losses = 0;
-var numberGuesses = 10;
+var tries = 10;
 var guesses = [];
 
-document.onkeydown = function(event) {
-  var userGuess = event.key;
-    guesses.push(userGuess);
-    console.log(guesses);
-    document.getElementById("key-presses").innerHTML = (guesses);
-    if (computerGuess === userGuess) {
-        wins++;
-        document.getElementById("T-wins").innerHTML = "Wins: " + wins;
-     } else {
-        document.getElementById("T-losses").innerHTML = "Losses: " + losses;
-     }
-}
-// The app randomly picks a letter, and the user has to guess which letter the app chose. Put the following text on your page:
+var totalWins = document.getElementById("T-wins");
+var totalLosses = document.getElementById("T-losses");
+var tries = document.getElementById("remaining-guesses");
+var guessesSoFar = document.getElementById("key-presses");
+
+
 
 
 // The specific letters that the user typed. Display these until the user either wins or loses.)
+document.onkeydown = function(event) {
+  var userGuess = event.key;
 
-// When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+  // The app randomly picks a letter, and the user has to guess which letter the app chose. 
+var computerChoice = computerGuess[Math.floor(Math.random() * computerGuess.length)];
 
-// When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+    guesses.push(userGuess);
+    console.log(guesses);
+    document.getElementById("key-presses").innerHTML = (guesses);
+
+    // When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+
+    if (computerChoice === userGuess) {
+        wins++;
+     
+      // When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+    } else if (computerChoice !== userGuess){
+        losses++;
+     }
+     else {
+        tries--;
+     }
+
+     document.getElementById("T-wins").innerHTML = "Wins: " +wins;
+     document.getElementById("T-losses").innerHTML = "Losses: " +losses;
+     document.getElementById("remaining-guesses").innerHTML = tries;
+
+      // Display the wins/losses/remaining guesses and user guesses.
+
+      totalWins.textContent = "Wins: " + wins;
+      totalLosses.textContent = "Losses: " + losses;
+      
+}
